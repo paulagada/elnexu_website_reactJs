85838844b83c6dd7a1e4c1bf333571a1c5efde8b
Paul <paulagada075@gmail.com>
2025-08-27 23:04:24 +0100

    commit5

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: '/elnexu_website_reactJs/',
   resolve: {
     alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}]
   }
